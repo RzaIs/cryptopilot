51d31c6d8df016d814cfa41c8e39e4258c77692b
RzaIs <rzaismail82@gmail.com>
2023-03-04 23:05:32 +0400

    Bridge validation between micro-services implemented

--- ms-nest/src/user/user.controller.ts
+++ ms-nest/src/user/user.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Controller, Get, Response, UnauthorizedException, UseGuards } from "@nestjs/common";
 import { User } from "@prisma/client";
+import axios, { AxiosError } from "axios";
 import { GetUser } from "src/auth/auth.decorators";
 import { AuthStrategy } from "src/auth/auth.strategy";
+import { msaxios } from "src/helper/ms.axios";
 import { UserResponseModel } from "./user.models";
 import { UserService } from "./user.service";
 
@@ -14,5 +16,10 @@ export class UserController {
   @Get('me')
   async getSelfUser(@GetUser() user: User): Promise<UserResponseModel> {
     return new UserResponseModel(user)
+  }
+
+  @Get('example')
+  async getExample(): Promise<any> {
+    return (await msaxios(() => axios.get('http://ms-data:4040/example/data'))).data
   }
 }

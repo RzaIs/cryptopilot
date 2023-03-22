e4da92b0b4f4d8e4fb7fa94d756b992aba372c83
NijatAghali <nicat_agali2002@mail.ru>
2023-03-22 18:14:55 +0400

    start/end date filters

--- ms-data/indicators/ma.py
+++ ms-data/indicators/ma.py
@@ -1,17 +1,21 @@
 # Moving Average
 
 import yfinance as yf
+import datetime as dt
 import math
 
 coins = ['BTC-USD', 'ETH-USD', 'ADA-USD']
 interval = ['15m', '30m', '1h', '1d', '1w']
+
+end_date = dt.datetime.strptime("2023-01-01", "%Y-%m-%d").date()
+start_date = end_date - dt.timedelta(days=365) # for 1 year
 ticker, interval = coins[2], interval[3]
 day1, day2 = 5,150 # user input
 
-def calculate_moving_average(ticker, interval, day1, day2):
+def calculate_moving_average(ticker, start_date, end_date, interval, day1, day2):
     
     # Download historical data for any Crypto Coin (ticker)
-    data = yf.download(ticker, period = "1y", interval = interval)
+    data = yf.download(ticker, start = start_date, end = end_date, interval=interval)
 
     # Calculate the moving average values
     ma1 = data['Close'].rolling(window=day1).mean()

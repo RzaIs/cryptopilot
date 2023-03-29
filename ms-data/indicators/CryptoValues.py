ccf10ea94ba10ca150b874d2511056b49672ac3a
NijatAghali <nicat_agali2002@mail.ru>
2023-03-29 14:59:41 +0400

    all crypto values

--- ms-data/indicators/CryptoValues.py
+++ ms-data/indicators/CryptoValues.py
@@ -18,10 +18,19 @@ end_date = dt.datetime.strptime("2023-01-01", "%Y-%m-%d").date()
 start_date = end_date - dt.timedelta(days=365) # for 1 year
 ticker, interval = coins[2], interval[3]
 
-def value(titicker, start_date, end_date, intervalcker):
+def value(ticker, start_date, end_date, interval):
 
     data = yf.download(ticker, start = start_date, end = end_date, interval=interval)
 
-    return data['Close']
 
-print(value(ticker, start_date, end_date, interval))
+    output = {
+            'index': data.index,
+            'open': data['Open'],
+            'high': data['High'], 
+            'low': data['Low'],
+            'close': data['Close'], 
+            'adj_close': data['Adj Close'], 
+            'volume': data['Volume']
+        }
+        
+    return output
